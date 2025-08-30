a13e7dc406bb7fe21121659f17026b14ec01204d
smata <smatarek25@gmail.com>
2025-08-30 17:31:28 +0300

    implement update profile screen UI

---
+++ lib/widgets/text_form_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:movies_app/constants/app_theme.dart';
+
+class CustomizeTextFormField extends StatefulWidget {
+  final String defaultText;
+  final String iconName;
+
+  // final String name;
+  final IconData icon;
+
+  CustomizeTextFormField({
+    super.key,
+    required this.iconName,
+    // required this.name,
+    required this.icon,
+    required this.defaultText,
+  });
+
+  @override
+  State<CustomizeTextFormField> createState() => _CustomizeTextFormFieldState();
+}
+
+class _CustomizeTextFormFieldState extends State<CustomizeTextFormField> {
+  late final TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController(text: widget.defaultText);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      cursorColor: AppTheme.white,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Roboto',
+        color: AppTheme.white,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: Icon(widget.icon, color: AppTheme.white, size: 30),
+        // prefixIcon: SvgPicture.asset('assets/icons/${widget.iconName}.svg'),
+        filled: true,
+        fillColor: AppTheme.grey,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: AppTheme.grey),
+        ),
+      ),
+    );
+  }
+}
